dc2b312cbb2bee473d09c3d3ace0383c3e4a2442
Hiroya Hinomori <nexearth@gmail.com>
2022-10-02 12:42:29 +0900

    Initialc commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by hiroya-hinomori on 2022/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
