3c7ce2d4ec1038bef205c4297f2a66b9852b0392
MrSmart00 <nexearth@gmail.com>
2022-10-03 22:16:56 +0900

    Add: Firebase SDK

--- Sandbox/SandboxApp.swift
+++ Sandbox/SandboxApp.swift
@@ -7,12 +7,24 @@
 
 import SwiftUI
 import App
+import Firebase
 
 @main
 struct SandboxApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             RootView(with: .init(queue: .main))
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
